dcdc43eefd8bf9a506da110f5e13d8519c671294
ShiraBenShaul <shirab44@gmail.com>
2022-05-13 13:03:45 +0300

    First ui and placeholders

--- Assets/Mechanics/Movement/PlayerController.cs
+++ Assets/Mechanics/Movement/PlayerController.cs
@@ -15,11 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool reachedEndOfPath;
     [SerializeField] private float speed = 2;
     [SerializeField] private float nextWaypointDistance;
-    
     [SerializeField] private int side = 0;
-    [SerializeField] private bool move;
-    [SerializeField] private bool front;
-    
     [SerializeField] private bool flip = true;
     [SerializeField] private bool findDoor;
     private Animator playerAnimator;
@@ -63,8 +59,7 @@ public class PlayerController : MonoBehaviour
 
         if (reachedEndOfPath)
         {
-            move = false;
-            playerAnimator.SetBool("Move", move);
+            playerAnimator.SetInteger("side", side);
             _path = null;
         }
             
@@ -107,9 +102,6 @@ public class PlayerController : MonoBehaviour
 
     private void IndicateDirection(Vector3 target)
     {
-        move = true;
-        playerAnimator.SetBool("Move", move);
-        
         if (findDoor) return;
         var pos = transform.position;
         float angle = Mathf.Atan2
@@ -118,59 +110,37 @@ public class PlayerController : MonoBehaviour
         
         if (angle < 60 && angle > -90) //RIGHT
         {
-            side = 2;
+            side = 0;
+            playerAnimator.SetInteger("side", 2);
+            playerAnimator.SetTrigger("walkFront");
+            print("right");
             if (pos.y < 0)
-            {
-                front = (angle > -10);
                 flip = false;
-            }
             else
-            {
-                front = !(angle > -10);
-                 flip = true;
-            }
+                flip = true;
             GetComponent<SpriteRenderer>().flipX = flip;
         }
-        
-        
         else if (angle >= 60 && angle < 120) //UP
         {
-            front = pos.y < 0;
-            side = pos.y < 0 ? 0 : 1;
+            side = 1;
+            playerAnimator.SetInteger("side", side);
         }
         if (angle <= -60 && angle > -120) //DOWN
         {
-            front = !(pos.y < 0);
-            side = pos.y < 0 ? 1 : 0;
+            side = 0;
+            playerAnimator.SetInteger("side", side);
         }
-        else if (angle <= -90 || angle > 120) //LEFT
+        else if ((angle <= -90) || (angle > 120)) //LEFT
         {
-            side = 2;
-            if (pos.y < 0){
-                if (angle > 120 && angle < 180)
-                    front = true;
-                else
-                    front = false;
+            side = 0;
+            playerAnimator.SetInteger("side", 2);
+            playerAnimator.SetTrigger("walkFront");
+            if (pos.y < 0)
                 flip = true;
-            }
             else
-            {
-                if (angle > 120 && angle < 180)
-                    front = false;
-                else
-                    front = true;
                 flip = false;
-            }
             GetComponent<SpriteRenderer>().flipX = flip;
-
-            if (angle > 120 && angle < 180)
-                front = false;
-            else
-                front = true;
-
         }
-        playerAnimator.SetInteger("Side", side);
-        playerAnimator.SetBool("Front", front);
     }
     
     
@@ -189,12 +159,11 @@ public class PlayerController : MonoBehaviour
     {
         if (other.CompareTag("Door"))
         {
+            print(WorldsManagerToturial.onLeft);
             if (WorldsManagerToturial.onLeft && other.transform.position.x <= 0)
             {
-                front = false;
                 side = 1;
-                playerAnimator.SetInteger("Side", side);
-                playerAnimator.SetBool("Front", front);
+                playerAnimator.SetInteger("side", side);
                 findDoor = true;
                 StartCoroutine(waitAndLoad("Level2"));
             }
