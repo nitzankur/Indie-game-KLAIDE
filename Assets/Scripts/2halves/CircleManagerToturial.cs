dc2bc5cd89b92af3e25df8b3374b49348a4ad85d
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-05-30 10:00:29 +0300

    with circle color in level 0

---
+++ Assets/Scripts/2halves/CircleManagerToturial.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleManagerToturial : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer right, left;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(WorldsManagerToturial.onLeft)
+        {
+            left.color = Color.black;
+            right.color = Color.white;
+        }
+        else
+        {
+            right.color = Color.black;
+            left.color = Color.white;
+        }
+        
+    }
+}
