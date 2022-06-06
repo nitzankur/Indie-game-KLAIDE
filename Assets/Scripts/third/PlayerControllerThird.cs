24da73186aad5e044398475432afff53253fe3ff
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-06-06 14:40:28 +0300

    with build

--- Assets/Scripts/third/PlayerControllerThird.cs
+++ Assets/Scripts/third/PlayerControllerThird.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Pathfinding;
-using UnityEditor.Animations;
+
 
 public class PlayerControllerThird : MonoBehaviour
 {
