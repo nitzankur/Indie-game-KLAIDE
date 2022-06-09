0577e89a49ca6ef73d38516b6f47313e0890c6d0
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-06-09 14:53:59 +0300

    with dix fish and flower particle system

--- Assets/Scripts/fishGenrate.cs
+++ Assets/Scripts/fishGenrate.cs
@@ -6,15 +6,12 @@ public class fishGenrate : MonoBehaviour
 {
     [SerializeField] private GameObject fish1, fish2;
     [SerializeField] private Vector3 pos;
-    private List<GameObject> fishList;
+    private List<GameObject> fishList = new List<GameObject>();
     private IEnumerator _coro;
     // Start is called before the first frame update
     void Start()
     {
-        _coro = GanerateFish1();
-        StartCoroutine(_coro);
-        // fishList.Add(fish1);
-        // fishList.Add(fish2);
+        StartCoroutine(waitAndLoad());
     }
 
     // Update is called once per frame
@@ -22,12 +19,21 @@ public class fishGenrate : MonoBehaviour
     {
         while (true)
         {
+            fishList.Add(fish1);
+            fishList.Add(fish2);
             var waitFor = Random.Range(5, 7);
-            var randFish = Random.Range(1, 2);
-            // var fish = fishList[randFish];
+            int randFish = Random.Range(0, 2);
+            var fish = fishList[randFish];
             yield return new WaitForSeconds(waitFor);
-            Instantiate(fish1, pos, Quaternion.identity, transform);   
+            Instantiate(fish, pos, Quaternion.identity, transform);   
         }
     }
-    
+
+    IEnumerator waitAndLoad()
+    {
+        yield return new WaitForSeconds(1);
+        _coro = GanerateFish1();
+        StartCoroutine(_coro);
+    }
+
 }
